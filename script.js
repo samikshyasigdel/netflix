51be7f2e4f27aa12b4b0552fcde3b658761e743d
samikshyasigdel <samikshyasigdel58@gmail.com>
2023-05-15 16:10:51 +0545

    initial commit

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ document.addEventListener("DOMContentLoaded", function handleCollapse() {
   let coll = document.getElementsByClassName("collapsible");
   let i;
 
-  // for (i = 0; i < coll.length; i++) {
-  //   coll[i].addEventListener("click", function () {
-  //     this.classList.toggle("active");
-  //     console.log("hello");
-  //     var content = this.nextElementSibling;
-  //     if (content.style.display === "block") {
-  //       content.style.display = "none";
-  //     } else {
-  //       content.style.display = "block";
-  //     }
-  //   });
-  // }
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      console.log("hello");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
   // var coll = document.getElementsByClassName("collapsible");
   // var i;
 
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function handleCollapse() {
       if (content.style.maxHeight) {
         content.style.maxHeight = null;
       } else {
-        content.style.maxHeight = content.scrollHeight + "rem";
+        content.style.maxHeight = content.scrollHeight + "px";
       }
     });
   }
