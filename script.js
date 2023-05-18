0adaa47a329765b98d821985462606bad2fd88a9
samikshyasigdel <samikshyasigdel58@gmail.com>
2023-05-18 20:30:38 +0545

    updated

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ document.addEventListener("DOMContentLoaded", function handleCollapse() {
   let coll = document.getElementsByClassName("collapsible");
   let i;
 
+  // for (i = 0; i < coll.length; i++) {
+  //   coll[i].addEventListener("click", function () {
+  //     this.classList.toggle("active");
+  //     console.log("hello");
+  //     var content = this.nextElementSibling;
+  //     if (content.style.display === "block") {
+  //       content.style.display = "none";
+  //     } else {
+  //       content.style.display = "block";
+  //     }
+  //   });
+  // }
+  // var coll = document.getElementsByClassName("collapsible");
+  // var i;
+
   for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function () {
       this.classList.toggle("active");
@@ -11,22 +26,13 @@ document.addEventListener("DOMContentLoaded", function handleCollapse() {
         content.style.display = "none";
       } else {
         content.style.display = "block";
-      }
-    });
-  }
-  // var coll = document.getElementsByClassName("collapsible");
-  // var i;
-
-  for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-      this.classList.toggle("active");
-      console.log("hello");
-      var content = this.nextElementSibling;
-      if (content.style.maxHeight) {
-        content.style.maxHeight = null;
-      } else {
         content.style.maxHeight = content.scrollHeight + "px";
       }
+      // if (content.style.maxHeight) {
+      //   content.style.maxHeight = null;
+      // } else {
+      //   content.style.maxHeight = content.scrollHeight + "px";
+      // }
     });
   }
 });
